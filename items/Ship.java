3f9720194fc39ea93b04d973b4cf11e5abda9294
rburns31 <rburns31@gatech.edu>
2014-09-23 12:28:51 -0400

    Base stuff set-up

---
+++ items/Ship.java
@@ -0,0 +1,13 @@
+package spacetrader.items;
+
+/**
+ *
+ * @author Ryan Burns
+ */
+public class Ship {
+    private String cargoBay;
+    private String hull;
+    private String type;
+    private String gadget;
+    private String shield;
+}
